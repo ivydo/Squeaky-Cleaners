115e9192232667e1bd21f3d6e158657185789d94
Hilliard Stamper <hilliard.stamper@att.net>
2022-01-23 12:28:24 -0500

    updated routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
-
+const dashboardRoute = require('./dashboard-route.js');
+const homeRoutes = require('./home-routes.js');
+router.use('/', homeRoutes);
 
 
 
 router.use('/api', apiRoutes);
-
+router.use('/dashboard', dashboardRoute);
 
 router.use((req, res) => {
     res.status(404).end();
