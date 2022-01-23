11deaea40d92d935e9cde03064b3440bd87ec532
Ivy Do <ivy.do2011@gmail.com>
2022-01-23 15:41:14 -0500

    login routes working

--- controllers/dashboard-route.js
+++ controllers/dashboard-route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Maid, User, Review } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     Maid.findAll({
