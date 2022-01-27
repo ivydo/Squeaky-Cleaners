a20baf54c99213e4411fb79e4bfc94ffdd2540d4
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-01-27 08:31:56 -0500

    combined get code for maids and reviews, loads with zero errors, but review data is not showing on dashboard page

--- controllers/dashboard-route.js
+++ controllers/dashboard-route.js
@@ -1,17 +1,15 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Maid, User, Review } = require('../models');
+const { Maid,  User,  Review} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  // Wrapping in try/catch in order to handle any database errors.
   try {
-    // Query database for all Maids.
     const maidRows = await Maid.findAll({
       attributes: [
         'id',
         'name',
-        
+        'schedule',
       ],
       include: [
         {
@@ -24,18 +22,16 @@ router.get('/', async (req, res) => {
         },
       ]
     });
-​
-    // Query database for all Reviews for the currently authenticated user.
+
     const reviewRows = await Review.findAll({
       where: {
-        // User id connected to session.
-        user_id: req.session.user_id
+        user_id: req.session.id
       },
       attributes: [
         'id',
         'title',
         'review_text',
-        'maid_id',
+        'maid_id'
       ],
       include: [
         {
@@ -45,47 +41,46 @@ router.get('/', async (req, res) => {
       ],
     })
 
-    // Clean up Sequelize records so we can pass them into Handlebars.
     const maids = maidRows.map(maid => maid.get({ plain: true }));
     const reviews = reviewRows.map(review => review.get({ plain: true }));
 
-    // Render dashboard.handlebars, passing in maids and reviews data.
-    res.render(
-      'dashboard',
-      {
-        maids,
-        reviews,
-        loggedIn: true,
-        style: "dashboard.css"
-      });
-  } catch (error) {
+    res.render ('dashboard', {
+      maids,
+      reviews,
+      loggedIn: true,
+      style: "dashboard.css"
+    });
+  }
+  catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 });
 
-  //to edit review from user logged in
-  router.get('/edit/:id', (req, res) => {
-    Review.findByPk(req.params.id, {
+  
+
+//to edit review from user logged in
+router.get('/edit/:id', (req, res) => {
+  Review.findByPk(req.params.id, {
       attributes: [
         'id',
         'title',
         'review_text',
       ],
-      include: [
-        {
-          model: Maid,
-          attributes: ['id', 'name'],
-          // include: {
-          //   model: User,
-          //   attributes: ['username']
-          // }
-        },
-      ]
+      include: [{
+        model: Maid,
+        attributes: ['id', 'name'],
+        // include: {
+        //   model: User,
+        //   attributes: ['username']
+        // }
+      }, ]
     })
     .then(dbReviewData => {
       if (dbReviewData) {
-        const review = dbReviewData.get({ plain: true });
+        const review = dbReviewData.get({
+          plain: true
+        });
 
         res.render('edit-review', {
           review,
@@ -98,7 +93,7 @@ router.get('/', async (req, res) => {
     .catch(err => {
       res.status(500).json(err);
     });
-  });
+});
 
-  
-  module.exports = router;
+
+module.exports = router;
